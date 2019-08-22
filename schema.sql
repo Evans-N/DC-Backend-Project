f5e51e5eabe78a084ca2f492eb602f5c435e4d78
Anthony Pellingra <anthonypellingra@gmail.com>
2019-08-22 12:14:42 -0400

    created schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+create table users (
+    id serial primary key,
+    name VARCHAR(30) not null,
+    email varchar(25) not null,
+    phone INTEGER not null,
+    picture VARCHAR(100) not null
+);
+
+create table trips (
+    id serial primary key,
+    name VARCHAR(50) not null,
+    city varchar(30) not null,
+    country varchar (20) not null,
+    start_date DATE not null,
+    end_date DATE not null,
+    creator_id INTEGER REFERENCES users(id),
+    description VARCHAR(250)
+);
+
+create table comments (
+    id serial primary key,
+    poster_id integer REFERENCES users(id),
+    trip_id integer references trips(id),
+    message varchar(1000)
+);
+
+create table attendance (
+    id serial primary key,
+    user_id integer REFERENCES users(id),
+    trip_id integer references trips(id)
+);
