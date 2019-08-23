ec12e57009cb930a1886dae6a767d4bb50fe3ff3
daniel-farlow <52146855+daniel-farlow@users.noreply.github.com>
2019-08-23 15:56:35 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ const db = require('../db');
 const bcrypt = require('bcrypt');
 const expressSession = require('express-session');
 require('dotenv');
-// const sessionOptions = {
-//   secret: process.env.SESSION_SECRET,
-//   resave: false,
-//   saveUninitialized: false
-// }
-// router.use(expressSession(sessionOptions));
+const sessionOptions = {
+   secret: process.env.SESSION_SECRET,
+   resave: false,
+   saveUninitialized: false
+}
+router.use(expressSession(sessionOptions));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
