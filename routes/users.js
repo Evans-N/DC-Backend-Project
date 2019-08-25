75093dc100c398e9052ee3a27e07f2cd5fde6601
Ian Miller <ian_c_miller@icloud.com>
2019-08-25 12:06:46 -0400

    adds funcationality to create trip page thorugh the creatTripProcess POST route in users.js

--- routes/users.js
+++ routes/users.js
@@ -89,18 +89,30 @@ router.get('/tripCreate', (req,res,next) => {
 });
 
 router.post('/tripCreateProcess', (req,res,next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const city = req.body.city;
+  const country = req.body.country;
+  const start_date = req.body.start_date;
+  const end_date = req.body.end_date;
+  const creator_id = req.session.userObject.id;
+  const description = req.body.description;
+  
+  // const createTripQuery = 
+  createTrip()
+  function createTrip(){
   const createTripQuery = `
-    INSERT INTO trips
-      (name, city, country, start_date, end_date, creator_id, description)
-    VALUES
-      ($1,$2,$3,$4,$5,$6,$7)
-      returning id
+  INSERT INTO trips
+    (name, city, country, start_date, end_date, creator_id, description)
+  VALUES
+    ($1,$2,$3,$4,$5,$6,$7)
+    returning id
   `
-  db.one(createTripQuery,[name, city, country, start_date, end_date, req.session.userObject.id, description]).then((resp)=>{
-    res.json({
-      msg: "Trip Created!"
-    })
-  })
+  db.one(createTripQuery,[name, city, country, start_date, end_date, creator_id, description])
+  .then((resp)=>{
+        res.redirect('/users/myProfile')
+      })
+  }
 });
 
 router.get('/myTrips', (req,res,next) => {
