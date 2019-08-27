54ab0885be8d72fb17fffbd446e65638b55887e7
Anthony Pellingra <anthonypellingra@gmail.com>
2019-08-27 13:31:42 -0400

    last minute

--- routes/users.js
+++ routes/users.js
@@ -211,14 +211,16 @@ router.get('/userProfiles/:userId', (req,res,next) => {
               userData: userDataData,
               userTrips: userTripsData,
               userTripsCreated: userTripsCreatedData,
-              userTripsAttended: userTripsAttendedData
+              userTripsAttended: userTripsAttendedData,
+              myPage: false
             })
      
         } else {
             res.render('newProfile', {
             userData: userDataData,
             userTripsCreated: userTripsCreatedData,
-            userTripsAttended: userTripsAttendedData
+            userTripsAttended: userTripsAttendedData,
+            myPage: false
           })
           }
         })
